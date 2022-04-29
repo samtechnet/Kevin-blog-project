995f692b223a66138bc8c60b09a0a37871c0d9f0
SAMTECHNET <samlaja1292@gmail.com>
2022-04-29 11:22:38 +0100

    created project structure and db user model

---
+++ src/models/user.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        emial: {
+            type: String,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: "user",
+            enum: ["user", "admin"],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        verificationCode: {
+            type: Number,
+        },
+        isEmailVerified: {
+            type: Boolean,
+            defualt: false,
+        },
+        passwordResetCode: {
+            type: String,
+        }
+    },
+    { timestamps: true }
+);
+
+export default model("User", UserSchema);

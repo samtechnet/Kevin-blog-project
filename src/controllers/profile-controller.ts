6d913b45d0093818f3c1137af1d450b05c8a7d9d
SAMTECHNET <samlaja1292@gmail.com>
2022-04-30 13:37:25 +0100

    created and exported profile-controller for the profile model

---
+++ src/controllers/profile-controller.ts
@@ -0,0 +1,40 @@
+import User from "../models/user";
+import express, { Request, Response } from "express";
+
+const updateOne = async (req: Request, res: Response) => {
+  try {
+    await User.findByIdAndUpdate(req.params.id, req.body);
+    return res.status(201).json({
+      message: "Item successfully updated",
+      success: true,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+const getOne = async (req: Request, res: Response) => {
+  try {
+    const item = await User.findById(req.params.id);
+    if (item) {
+      return res.status(200).json(item);
+    }
+    return res.status(404).json({
+      message: "Item not found",
+      success: false,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+export default {
+  updateOne,
+  getOne,
+};

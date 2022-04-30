f0cff7eade804446f4a924884dd4b48318bcf045
SAMTECHNET <samlaja1292@gmail.com>
2022-04-30 13:18:13 +0100

    created and exported video-controller for the video model

---
+++ src/controllers/video-controller.ts
@@ -0,0 +1,138 @@
+import Video from "../models/video";
+import paginate from "express-paginate";
+import express, { Request, Response } from "express";
+import Comment from "../models/comment";
+
+const addOne = async (req: Request, res: Response) => {
+  try {
+    const newRecord = new Video({
+      ...req.body,
+      createdBy: req.user.id,
+    });
+    await newRecord.save();
+    return res.status(201).json({
+      message: "Item successfully created",
+      success: true,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+const removeOne = async (req: Request, res: Response) => {
+  try {
+    const deleted = await Video.findByIdAndDelete(req.params.id);
+    if (!deleted) {
+      return res.status(404).json({
+        message: "Item not found",
+        success: false,
+      });
+    }
+    return res.status(204).json({
+      message: "Item successfully deleted",
+      success: true,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+const updateOne = async (req: Request, res: Response) => {
+  try {
+    await Video.findByIdAndUpdate(req.params.id, req.body);
+    return res.status(201).json({
+      message: "Item successfully updated",
+      success: true,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+const getAll = async (req: Request, res: Response) => {
+  //let itemCount: Number;
+  try {
+    const [results, itemCount] = await Promise.all([
+      Video.find({})
+        .sort({ createdAt: -1 })
+        .limit(req.query.limit)
+        .skip(req.skip)
+        .lean()
+        .exec(),
+      Video.count({}),
+    ]);
+    const pageCount = Math.ceil(itemCount / req.query.limit);
+    return res.status(201).json({
+      object: "List",
+      has_more: paginate.hasNextPages(req)(pageCount),
+      data: results,
+      pageCount,
+      itemCount,
+      currentPage: req.query.page,
+      pages: paginate.getArrayPages(req)(3, pageCount, req.query.page),
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+const getOne = async (req: Request, res: Response) => {
+  try {
+    let item = await Video.findByIdAndUpdate(req.params.id, {
+      $inc: { viewsCount: 1 },
+    });
+    if (item) {
+      return res.status(200).json(item);
+    }
+    return res.status(404).json({
+      message: "Item not found",
+      success: false,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+ 
+const getTopVideos = async (req: Request, res: Response) => {
+  //let itemCount: Number;
+  try {
+    let result = await Video.find({})
+      .sort({ viewsCount: -1 })
+      .limit(3)
+      .lean()
+      .exec();
+
+    return res.status(201).json({
+      data: result,
+    });
+  } catch (error: any) {
+    return res.status(500).json({
+      message: error.message,
+      success: false,
+    });
+  }
+};
+
+export default {
+  addOne,
+  removeOne,
+  updateOne,
+  getAll,
+  getOne,
+  getTopVideos,
+};

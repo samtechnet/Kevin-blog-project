628a91ecfaa02ada2af09569dae5ac078769c7d7
SAMTECHNET <samlaja1292@gmail.com>
2022-05-02 02:42:51 +0100

    adding execute permission

--- git.js
+++ git.js
@@ -1,26 +1,30 @@
+/**
+ *script to use in package.json
+ *  node git.js real update message
+ */
 import { exec } from "child_process";
-
-const args = process.argv;
-//console.log(args)
+let args = process.argv;
 args.splice(0, 2);
 let str = args.join(' ');
-console.log('kkk', str);
-exec('git add .');
+console.log(str);
+exec('git add .', cbadd);
 
-// function cbadd(err, strout, sdtin) { 
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     //it worked
-//     exec(`git commit -m "${str}"`, cbcommit);
-// }
+function cbadd(err, strout, sdtin) { 
+    if (err) {
+        console.log(err);
+        return;
+    }
+    //it worked
+    exec(`git commit -m "${str}"`, cbcommit);
+}
 
-// function cbcommit(err, strout, sdtin) { 
-//     if (err) {
-//         console.log(err);
-//         return;
-//     }
-//     //it worked
-//     exec('done');
-// }
+function cbcommit(err, strout, sdtin) { 
+    if (err) {
+        console.log(err);
+        return;
+    }
+    //it worked
+    console.log('done');
+}
+
+// command to run your script will be: npm run gitty -- your commit message
